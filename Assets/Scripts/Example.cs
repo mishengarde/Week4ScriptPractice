fbd4ba3b1f8b1ea61044dc8ba1f7f05c033779d5
Michelle <mun.michelless@gmail.com>
2018-09-19 19:35:06 +1000

    Created first script

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+
+    public bool trueOrFalse;
+    public float decimalNumber;
+    private float speed = 100.0f;
+    public string firstName = "Meeshel";
+    public Transform RemainsCylinder;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Hello world");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(RemainsCylinder, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
