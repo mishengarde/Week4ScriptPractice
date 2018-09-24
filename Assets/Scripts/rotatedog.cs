0e354ce925b0d30d43a5a7b61057bab2b2466acd
Michelle <mun.michelless@gmail.com>
2018-09-24 18:50:59 +1000

    Dog stuff

---
+++ Assets/Scripts/rotatedog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatedog : MonoBehaviour
+{
+    public void Rotate(float sliderValue)
+    {
+        transform.eulerAngles = new Vector3(0, 0, sliderValue);
+    }
+
+}
+
